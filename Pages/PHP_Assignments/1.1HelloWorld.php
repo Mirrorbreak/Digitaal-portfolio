39354b600d461a00b2610ea1d4008c2429792900
Pascal <m1rr0rbr34k@gmail.com>
2017-05-29 10:36:43 +0200

    PHP addition

---
+++ Pages/PHP_Assignments/1.1HelloWorld.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+  <h1>wew PHP en HTML</h1>
+  <p>Ik heb momenteel geen andere HTML website dus ik vul dit wel met wat HTML</p>
+<?php
+date_default_timezone_set('UTC');
+
+echo "Hello world! de datum is ", date('d-m-Y');
+?>
+
+</body>
+</html>
---
+++ Pages/PHP_Assignments/1.2Variables.php
@@ -0,0 +1,8 @@
+<?php
+date_default_timezone_set('UTC');
+$strlen = "";
+$strrev = "";
+
+
+
+?>
